e34d9153564c05c4dac7c787b58614e75e2bda2c
S3rgioHe <54727461+S3rgioHe@users.noreply.github.com>
2021-09-21 17:57:54 -0300

    Atualização

--- Avançando com PHP/banco.php
+++ Avançando com PHP/banco.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'funcoes.php';
+
 $contasCorrentes = [
     '183-838-298-09' => [
         'nome' => 'Freddie',
@@ -31,57 +33,13 @@ $contasCorrentes = [
     ],
 ];
 
-function listarUsers() {
-    global $contasCorrentes;
-    foreach($contasCorrentes as $conta) {
-        echo $conta['nome'] . PHP_EOL;
-    }
-}
-
-function sacarDaConta(string $identifier, float $value)
-{    
-    global $contasCorrentes;
-
-    if (strlen($identifier) != 14) {
-        foreach($contasCorrentes as $key => $contas) {
-            if(is_string($identifier)) {
-                if($contas['nome'] == $identifier) {
-                    $cpf = $key;
-                }
-            } else {
-                echo "Valor inválido";
-            }
-        }
-    } else {
-        $cpf = $identifier;
-    }
-
-    echo 'OLá ' . $contasCorrentes[$cpf] ['nome'] . ' sua conta tem o saldo de: ' . $contasCorrentes[$cpf] ['saldo'] . PHP_EOL;
-    if ($value > $contasCorrentes[$cpf] ['saldo']) {
-        echo "impossível de realizar esse saque, o valor máximo disponível é de: " . $contasCorrentes[$cpf] ['saldo'] . PHP_EOL;
-    } else {
-        $contasCorrentes[$cpf] ['saldo'] -= $value;https://cursos.alura.com.br/formacao-desenvolvedor-php
-        echo "O saque foi realizado com sucesso, seu novo saldo é de: " . $contasCorrentes[$cpf] ['saldo'] . PHP_EOL;
-    }
-}
-
-
-function depositarNaConta(string $identifier, float $value) 
-{
-    global $contasCorrentes;
-    echo "Olá " . $contasCorrentes[$identifier] ['nome'] . PHP_EOL; 
-    if($value > 0) {
-        $contasCorrentes[$identifier] ['saldo'] += $value;
-        echo "Seu saldo era de: " . ($contasCorrentes[$identifier] ['saldo'] - $value) . PHP_EOL . "Agora: " . $contasCorrentes[$identifier] ['saldo'] . PHP_EOL; 
-    
-    } else {
-        echo "Input errado!" . PHP_EOL;
-    }
-}
-
 listarUsers();
+PHP_EOL;
 sacarDaConta('Freddie', 1000);
-depositarNaConta('183-838-298-09', -5000);
+PHP_EOL;
+depositarNaConta('183-838-298-09', 5000);
+
+tudoMaiuscula('183-838-298-09');
 
 
 
