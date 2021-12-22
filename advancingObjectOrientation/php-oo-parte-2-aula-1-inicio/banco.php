800b6e6133dd12df36efb618ea65ce4dced9dfe8
Sergio-He <84047131+Sergio-He@users.noreply.github.com>
2021-12-22 10:46:51 -0300

    feat: added "Endereco" class parameter

--- advancingObjectOrientation/php-oo-parte-2-aula-1-inicio/banco.php
+++ advancingObjectOrientation/php-oo-parte-2-aula-1-inicio/banco.php
@@ -4,19 +4,22 @@ require_once 'src/Conta.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
 
-$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
+
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', new Endereco('cidade exemplo', 'bairro exemplo', 'rua exemplo', 'a1'));
 $primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
-$primeiraConta->saca(300); // isso é ok
+$primeiraConta->saca(300);
 
 echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
+$enderecoPatriaGomes = new Endereco('Caruaru-pe', 'Boa vista', 'Rua Ademar Ribeiro Costa', '12');
+
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $enderecoPatriaGomes);
 $segundaConta = new Conta($patricia);
 var_dump($segundaConta);
 
-$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg'));
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Gomes', $enderecoPatriaGomes));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
